0dd8c2378bd3f1c694d84a47a448493de0786e20
Chaz_B <chaboffe@gmail.com>
2021-07-19 22:04:35 -0700

    Initial lode

---
+++ Lab06and07 The Zoo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab06and07_The_Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
